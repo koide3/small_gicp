3752a64d2175950a246015b1eebde36af8ad25b3
k.koide <k.koide@aist.go.jp>
2024-03-26 19:09:29 +0900

    vgicp_omp

--- src/odometry_benchmark_small_vgicp_omp.cpp
+++ src/odometry_benchmark_small_vgicp_omp.cpp
@@ -3,15 +3,16 @@
 #include <small_gicp/factors/gicp_factor.hpp>
 #include <small_gicp/points/point_cloud.hpp>
 #include <small_gicp/ann/kdtree_omp.hpp>
+#include <small_gicp/ann/gaussian_voxelmap.hpp>
 #include <small_gicp/util/normal_estimation_omp.hpp>
 #include <small_gicp/registration/reduction_omp.hpp>
 #include <small_gicp/registration/registration.hpp>
 
 namespace small_gicp {
 
-class SmallGICPOnlineOdometryEstimationOMP : public OnlineOdometryEstimation {
+class SmallVGICPOnlineOdometryEstimationOMP : public OnlineOdometryEstimation {
 public:
-  SmallGICPOnlineOdometryEstimationOMP(const OdometryEstimationParams& params) : OnlineOdometryEstimation(params), T(Eigen::Isometry3d::Identity()) {}
+  SmallVGICPOnlineOdometryEstimationOMP(const OdometryEstimationParams& params) : OnlineOdometryEstimation(params), T(Eigen::Isometry3d::Identity()) {}
 
   Eigen::Isometry3d estimate(const PointCloud::Ptr& points) override {
     Stopwatch sw;
@@ -20,24 +21,27 @@ public:
     auto tree = std::make_shared<KdTreeOMP<PointCloud>>(points, params.num_threads);
     estimate_covariances_omp(*points, *tree, 10, params.num_threads);
 
+    auto voxelmap = std::make_shared<GaussianVoxelMap>(params.voxel_resolution);
+    voxelmap->insert(*points);
+
     if (target_points == nullptr) {
       target_points = points;
-      target_tree = tree;
+      target_voxelmap = voxelmap;
       return T;
     }
 
     Registration<GICPFactor, ParallelReductionOMP, DistanceRejector, LevenbergMarquardtOptimizer> registration;
     registration.rejector.max_dist_sq = params.max_correspondence_distance * params.max_correspondence_distance;
-    registration.reduction.num_threads = params.num_threads;
 
-    auto result = registration.align(*target_points, *points, *target_tree, Eigen::Isometry3d::Identity());
+    auto result = registration.align(*target_voxelmap, *points, *target_voxelmap, Eigen::Isometry3d::Identity());
 
     sw.stop();
     reg_times.push(sw.msec());
 
     T = T * result.T_target_source;
+
     target_points = points;
-    target_tree = tree;
+    target_voxelmap = voxelmap;
 
     return T;
   }
@@ -50,12 +54,12 @@ private:
   Summarizer reg_times;
 
   PointCloud::Ptr target_points;
-  KdTreeOMP<PointCloud>::Ptr target_tree;
+  GaussianVoxelMap::Ptr target_voxelmap;
 
   Eigen::Isometry3d T;
 };
 
-static auto small_gicp_omp_registry =
-  register_odometry("small_gicp_omp", [](const OdometryEstimationParams& params) { return std::make_shared<SmallGICPOnlineOdometryEstimationOMP>(params); });
+static auto small_vgicp_omp_registry =
+  register_odometry("small_vgicp_omp", [](const OdometryEstimationParams& params) { return std::make_shared<SmallVGICPOnlineOdometryEstimationOMP>(params); });
 
 }  // namespace small_gicp
