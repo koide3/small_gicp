d9b0ead14b78e13019fa9133fb300ab7d5913ac9
k.koide <k.koide@aist.go.jp>
2024-04-16 19:16:12 +0900

    IncrementalVoxelMap

--- src/python/voxelmap.cpp
+++ src/python/voxelmap.cpp
@@ -15,37 +15,56 @@
 namespace py = pybind11;
 using namespace small_gicp;
 
-void define_voxelmap(py::module& m) {
-  // GaussianVoxelMap
-  py::class_<GaussianVoxelMap>(m, "GaussianVoxelMap")  //
-    .def(py::init<double>())
+template <typename VoxelMap, bool has_normals, bool has_covs>
+auto define_class(py::module& m, const std::string& name) {
+  py::class_<VoxelMap> vox(m, name.c_str());
+  vox.def(py::init<double>())
     .def(
       "__repr__",
-      [](const GaussianVoxelMap& voxelmap) {
+      [=](const VoxelMap& voxelmap) {
         std::stringstream sst;
-        sst << "small_gicp.GaussianVoxelMap (" << 1.0 / voxelmap.inv_leaf_size << " m / " << voxelmap.size() << " voxels)" << std::endl;
+        sst << "small_gicp." << name << "(" << 1.0 / voxelmap.inv_leaf_size << " m / " << voxelmap.size() << " voxels)" << std::endl;
         return sst.str();
       })
-    .def("__len__", [](const GaussianVoxelMap& voxelmap) { return voxelmap.size(); })
-    .def("size", &GaussianVoxelMap::size)
+    .def("__len__", [](const VoxelMap& voxelmap) { return voxelmap.size(); })
+    .def("size", &VoxelMap::size)
     .def(
       "insert",
-      [](GaussianVoxelMap& voxelmap, const PointCloud& points, const Eigen::Matrix4d& T) { voxelmap.insert(points, Eigen::Isometry3d(T)); },
+      [](VoxelMap& voxelmap, const PointCloud& points, const Eigen::Matrix4d& T) { voxelmap.insert(points, Eigen::Isometry3d(T)); },
       py::arg("points"),
       py::arg("T") = Eigen::Matrix4d::Identity())
     .def(
       "set_lru",
-      [](GaussianVoxelMap& voxelmap, size_t horizon, size_t clear_cycle) {
+      [](VoxelMap& voxelmap, size_t horizon, size_t clear_cycle) {
         voxelmap.lru_horizon = horizon;
         voxelmap.lru_clear_cycle = clear_cycle;
       },
       py::arg("horizon") = 100,
       py::arg("clear_cycle") = 10)
-    .def(
-      "voxel_points",
-      [](const GaussianVoxelMap& voxelmap) -> Eigen::MatrixXd {
-        auto points = traits::voxel_points(voxelmap);
-        return Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(points[0].data(), points.size(), 4);
-      })
-    .def("voxel_covs", [](const GaussianVoxelMap& voxelmap) { return traits::voxel_covs(voxelmap); });
+    .def("voxel_points", [](const VoxelMap& voxelmap) -> Eigen::MatrixXd {
+      auto points = traits::voxel_points(voxelmap);
+      return Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(points[0].data(), points.size(), 4);
+    });
+
+  if constexpr (has_normals) {
+    vox.def("voxel_normals", [](const VoxelMap& voxelmap) -> Eigen::MatrixXd {
+      auto normals = traits::voxel_normals(voxelmap);
+      return Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(normals[0].data(), normals.size(), 4);
+    });
+  }
+
+  if constexpr (has_covs) {
+    vox.def("voxel_covs", [](const VoxelMap& voxelmap) -> Eigen::MatrixXd {
+      auto covs = traits::voxel_covs(voxelmap);
+      return Eigen::Map<const Eigen::Matrix<double, -1, -1, Eigen::RowMajor>>(covs[0].data(), covs.size(), 16);
+    });
+  }
+};
+
+void define_voxelmap(py::module& m) {
+  define_class<IncrementalVoxelMap<FlatContainerPoints>, false, false>(m, "IncrementalVoxelMap");
+  define_class<IncrementalVoxelMap<FlatContainerNormal>, true, false>(m, "IncrementalVoxelMapNormal");
+  define_class<IncrementalVoxelMap<FlatContainerCov>, false, true>(m, "IncrementalVoxelMapCov");
+  define_class<IncrementalVoxelMap<FlatContainerNormalCov>, true, true>(m, "FlatContainerNormalCov");
+  define_class<GaussianVoxelMap, false, true>(m, "GaussianVoxelMap");
 }
