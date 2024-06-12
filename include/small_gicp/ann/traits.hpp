70fab2387ad3a2890f989c4a6c21a0123e91fe6a
k.koide <k.koide@aist.go.jp>
2024-06-12 10:22:46 +0900

    fix exclude path for doxygen

--- include/small_gicp/ann/traits.hpp
+++ include/small_gicp/ann/traits.hpp
@@ -16,14 +16,15 @@ struct Traits;
 /// @param tree       Nearest neighbor search (e.g., KdTree)
 /// @param point      Query point
 /// @param k          Number of neighbors
-/// @param k_index    [out] Index of the nearest neighbor
-/// @param k_sq_dist  [out] Squared distance to the nearest neighbor
+/// @param k_indices  [out] Indices of k-nearest neighbors
+/// @param k_sq_dists [out] Squared distances to k-nearest neighbors
 /// @return Number of found neighbors
 template <typename T>
 size_t knn_search(const T& tree, const Eigen::Vector4d& point, size_t k, size_t* k_indices, double* k_sq_dists) {
   return Traits<T>::knn_search(tree, point, k, k_indices, k_sq_dists);
 }
 
+/// @brief Check if T has nearest_neighbor_search method.
 template <typename T>
 struct has_nearest_neighbor_search {
   template <typename U, int = (&Traits<U>::nearest_neighbor_search, 0)>
@@ -33,7 +34,7 @@ struct has_nearest_neighbor_search {
   static constexpr bool value = decltype(test((T*)nullptr))::value;
 };
 
-/// @brief Find the nearest neighbor.
+/// @brief Find the nearest neighbor. If Traits<T>::nearest_neighbor_search is not defined, fallback to knn_search with k=1.
 /// @param tree       Nearest neighbor search (e.g., KdTree)
 /// @param point      Query point
 /// @param k_index    [out] Index of the nearest neighbor
